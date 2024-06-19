2bef214c02d2ec8f0bce5e4f17191bab0074f52e
zetaxbyte <zetaxlinux@gmail.com>
2024-06-19 11:18:15 +0000

    fix error: incompatible integer to pointer conversion passing 'int' to parameter of type 'void *'

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -1173,10 +1173,14 @@ static const struct file_operations wakeup_sources_stats_fops = {
 
 static int __init wakeup_sources_debugfs_init(void)
 {
-	wakeup_sources_stats_dentry = debugfs_create_file("wakeup_sources",
-			S_IRUGO, NULL, NULL, &wakeup_sources_stats_fops);
-	debugfs_create_file("trace_marker", 0220, debugfs_create_dir("tracing", NULL), 1, NULL);
-	return 0;
+    int *value = kmalloc(sizeof(int), GFP_KERNEL);
+    if (value) {
+        *value = 1;
+        wakeup_sources_stats_dentry = debugfs_create_file("wakeup_sources",
+                        S_IRUGO, NULL, NULL, &wakeup_sources_stats_fops);
+        debugfs_create_file("trace_marker", 0220, debugfs_create_dir("tracing", NULL), value, NULL);
+    }
+    return 0;
 }
 
 postcore_initcall(wakeup_sources_debugfs_init);
